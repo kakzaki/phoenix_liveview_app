c29375f05970f2c5a130a04fe624c5bc7584e0cf
Zaki Mubarok <kakzaki@gmail.com>
2025-02-21 17:08:45 +0700

    init

--- lib/phoenix_liveview_app/application.ex
+++ lib/phoenix_liveview_app/application.ex
@@ -1,6 +1,4 @@
 defmodule PhoenixLiveviewApp.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -9,28 +7,17 @@ defmodule PhoenixLiveviewApp.Application do
   def start(_type, _args) do
     children = [
       PhoenixLiveviewAppWeb.Telemetry,
-      PhoenixLiveviewApp.Repo,
-      {DNSCluster, query: Application.get_env(:phoenix_liveview_app, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: PhoenixLiveviewApp.PubSub},
-      # Start the Finch HTTP client for sending emails
-      {Finch, name: PhoenixLiveviewApp.Finch},
-      # Start a worker by calling: PhoenixLiveviewApp.Worker.start_link(arg)
-      # {PhoenixLiveviewApp.Worker, arg},
-      # Start to serve requests, typically the last entry
+      # Start to serve requests
       PhoenixLiveviewAppWeb.Endpoint,
-
-      # Tambahkan Store di sini, tanpa komentar #
+      # Store for in-memory book management
       {PhoenixLiveviewApp.Books.Store, []}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: PhoenixLiveviewApp.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     PhoenixLiveviewAppWeb.Endpoint.config_change(changed, removed)
