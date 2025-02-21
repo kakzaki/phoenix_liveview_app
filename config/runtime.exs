584d3f72c975fc6e9404b87f483b0df93adda3f8
Zaki Mubarok <kakzaki@gmail.com>
2025-02-21 17:20:28 +0700

    init

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,21 +1,5 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
-
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/phoenix_liveview_app start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
   config :phoenix_liveview_app, PhoenixLiveviewAppWeb.Endpoint, server: true
 end
@@ -28,10 +12,8 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "phoenix-liveview-app.zeabur.app"
   port = String.to_integer(System.get_env("PORT") || "4000")
-
-  config :phoenix_liveview_app, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :phoenix_liveview_app, PhoenixLiveviewAppWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
@@ -40,37 +22,8 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base,
+    check_origin: ["https://phoenix-liveview-app.zeabur.app"],
     server: true
 
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :phoenix_liveview_app, PhoenixLiveviewAppWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :phoenix_liveview_app, PhoenixLiveviewAppWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
+  config :phoenix_liveview_app, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 end
