09b82e59604c872b6c4577c614e7822208c9617c
Zaki Mubarok <kakzaki@gmail.com>
2025-02-22 08:26:06 +0700

    port

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,7 +16,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :phoenix_liveview_app, PhoenixLiveviewAppWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port, scheme: "https"],
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
